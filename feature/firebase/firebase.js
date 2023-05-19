e18b5bf0b7652ea9b650ae44fa091133969277fa
stylishjoker <animetplink@gmail.com>
2023-05-19 09:54:20 +0700

    !

--- feature/firebase/firebase.js
+++ feature/firebase/firebase.js
@@ -5,8 +5,7 @@ import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCZNXQmy6l0DXuqA4_2-ejvgeifkqNOqnY",
-  // authDomain: "kfc-project-1292f.firebaseapp.com",
-  authDomain: "economic-kfc-git-dev-stylishjoker.vercel.app",
+  authDomain: "kfc-project-1292f.firebaseapp.com",
   projectId: "kfc-project-1292f",
   storageBucket: "kfc-project-1292f.appspot.com",
   messagingSenderId: "107513396962",
